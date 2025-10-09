275a6d683a0dd1ab8b780afd6bef10764be6c4f1
Francisco Josafá <josafa.lopes@inventione.com.br>
2025-10-09 10:59:46 -0300

    fix: corrige caminhode importação do arquivode roles

--- apps/api/src/http/routes/invites/create-invite.ts
+++ apps/api/src/http/routes/invites/create-invite.ts
@@ -6,6 +6,7 @@ import { z } from 'zod'
 import { prisma } from "@/lib/prisma";
 import { getUserPermissions } from "@/utils/get-user-permissions";
 import { roleSchema } from '@saas/auth/src/roles';
+// import { roleSchema } from '@saas/auth/src/roles';
 
 export async function createInvite(app: FastifyInstance) {
     app
