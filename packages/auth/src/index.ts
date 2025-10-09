5f32e6a38afe921c5949ac9209b65b59f45d89e5
Francisco Josafá <josafa.lopes@inventione.com.br>
2025-10-09 10:40:20 -0300

    fix: refatora appAbilitySchema

--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -14,6 +14,7 @@ import { inviteSubject } from './subjects/invite'
 import { billingSubject } from './subjects/billing'
 import { userSubject } from './subjects/user';
 
+
 const appAbilitiesSchema = z.union([
   projectSubject,
   userSubject,
