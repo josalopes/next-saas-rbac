3f78abe66d5f1e8c350272cb2a44a60925006047
Francisco Josafá <josafa.lopes@inventione.com.br>
2025-10-09 12:52:46 -0300

    fix: refatora arquivos tsup.config e tsconfig segundo orientação do ChatGpt

--- apps/api/tsup.config.ts
+++ apps/api/tsup.config.ts
@@ -1,9 +1,14 @@
 import { defineConfig } from 'tsup'
 
 export default defineConfig({
-    entry: ['src'],
+    entry: ['src/http/server.ts'],
+    outDir: 'dist',
+    target: 'node20',
+    format: ['cjs'],
     splitting: false,
     sourcemap: true,
     clean: true,
+    dts: false,
+    shims: false,
     noExternal: ['saas/auth', 'saas/env']
 })
