9682147f3d0b191d363740da4dbb03bb9aebde3f
Francisco Josafá <josafa.lopes@inventione.com.br>
2025-10-09 10:22:08 -0300

    fix: remove código comentado

--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -14,11 +14,6 @@ import { inviteSubject } from './subjects/invite'
 import { billingSubject } from './subjects/billing'
 import { userSubject } from './subjects/user';
 
-// export * from './models/organization'
-// export * from './models/project'
-// export * from './models/user'
-// export * from './subjects/roles'
-
 const appAbilitiesSchema = z.union([
   projectSubject,
   userSubject,
