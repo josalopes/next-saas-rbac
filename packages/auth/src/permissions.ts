b94b706bea1352946f5588d2656fb0ecf59289d1
Francisco Josafá <josafa.lopes@inventione.com.br>
2025-11-30 09:30:20 -0300

    chore: coloca todo o conteúdo de subject>index.ts em subject>permissions.ts e deixa o original comentado

--- packages/auth/src/permissions.ts
+++ packages/auth/src/permissions.ts
@@ -1,7 +1,44 @@
-import { AbilityBuilder } from "@casl/ability"
-import { AppAbility } from "./index"
-import { User } from "./models/user"
+import z from 'zod'
+import { 
+  createMongoAbility, 
+  CreateAbility, 
+  MongoAbility, 
+  AbilityBuilder 
+} from '@casl/ability'
+import { User } from './models/user'
+import { projectSubject } from './subjects/project';
+import { organizationSubject } from './subjects/organization'
+import { inviteSubject } from './subjects/invite'
+import { billingSubject } from './subjects/billing'
+import { userSubject } from './subjects/user';
+
+// export * from './models/organization'
+// export * from './models/project'
+// export * from './models/user'
+// export * from './roles'
+
+
+// import { AbilityBuilder } from "@casl/ability"
+// import { AppAbility } from "./index"
+// import { User } from "./models/user"
 import { Role } from "./roles"
+
+const appAbilitiesSchema = z.union([
+  projectSubject,
+  userSubject,
+  organizationSubject,
+  inviteSubject,
+  billingSubject,
+  z.tuple([
+    z.literal('manage'),
+    z.literal('all')
+  ])
+])
+
+type AppAbilities = z.infer<typeof appAbilitiesSchema>
+
+export type AppAbility = MongoAbility<AppAbilities>;
+export const createAppAbility = createMongoAbility as CreateAbility<AppAbility>;
 
 type PermissionsByRole = (
     user: User,
@@ -22,4 +59,26 @@ export const permissions: Record<Role, PermissionsByRole> = {
     BILLING: (_, { can }) => {
         can('manage', 'Billing')
     }
+}
+
+// *********************************************
+export function defineAbilityFor(user: User) {
+  const { can, cannot, build } = new AbilityBuilder(createAppAbility)
+
+  if (typeof permissions[user.role] !== 'function') {
+    throw new Error(`Permissions for role ${user.role} not found`)
+  }
+
+  permissions[user.role](user, { can, cannot })
+
+  const ability = build({
+    detectSubjectType(subject) {
+      return subject.__typename
+    },
+  })
+
+  ability.can = ability.can.bind(ability)
+  ability.cannot = ability.cannot.bind(ability)
+
+  return ability
 }
